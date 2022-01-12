2d858cdf0980d3f8a2beab4d9c6b5d4d84ed82c4
Prashanth Reddy <reddyprashanth610@gmail,com>
2022-01-12 13:24:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Gallery from './components/Gallery'
+import SearchBox from './components/SearchBox'
+
+const App = () => {
+    const [input, setInput] = useState("");
+    return (
+        <center>
+            <h1>Welcome to PhotoSearch App</h1>
+            <SearchBox input={input} setInput={setInput} />
+            <Gallery query={input} />
+        </center>
+    )
+}
+
+export default App
